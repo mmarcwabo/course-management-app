ad6ba12d33ca9d9d57d1c86aa936b438e9e13404
mmarcwabo <marcellin.wabo@gmail.com>
2021-03-26 07:04:21 +0100

    Add alertmessage to Utils class

--- libs/Utils.php
+++ libs/Utils.php
@@ -301,7 +301,7 @@ class Utils
   {
     echo '
   <div class="alert alert-' . $messageType . ' alert-dismissible fade show" role="alert">
-  <strong>'.$message.'</strong>You should check in on some of those fields below.
+  <strong>'.$message.'! </strong>Welcome to your dashboard <strong>'.$_SESSION["connectedUser"].'</strong> .
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
